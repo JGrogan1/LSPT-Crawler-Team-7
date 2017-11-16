1cbf218c8ea8cba2abfc53ad458e97975b21b3d4
Joe Grogan <grogaj@rpi.edu>
2017-11-16 15:06:39 -0500

    Junit

---
+++ test/CrawlerTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class CrawlerTest {
+
+    @Test
+    public void testCrawler() {
+        String args[] = {"cs.rpi.edu"};
+//        Crawler.main(args);
+    }
+}
