1c1c1177f73685992990c11cc919a70255046046
Joe Grogan <grogaj@rpi.edu>
2017-11-16 15:23:30 -0500

    crawls a link

--- src/Spider.java
+++ src/Spider.java
@@ -1,66 +1,65 @@
-import java.util.HashSet;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
-public class Spider {
-    private static final int MAX_PAGES_TO_SEARCH = 10;
-    private Set<String> pagesVisited = new HashSet<String>();
-    private List<String> pagesToVisit = new LinkedList<String>();
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Spider
+{
+    // We'll use a fake USER_AGENT so the web server thinks the robot is a normal web browser.
+    private static final String USER_AGENT =
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
+    private List<String> links = new LinkedList<String>();
+    private Document htmlDocument;
 
 
     /**
-     * Our main launching point for the Spider's functionality. Internally it creates spider legs
-     * that make an HTTP request and parse the response (the web page).
+     * This performs all the work. It makes an HTTP request, checks the response, and then gathers
+     * up all the links on the page. Perform a searchForWord after the successful crawl
      *
      * @param url
-     *            - The starting point of the spider
-     * @param searchWord
-     *            - The word or string that you are searching for
+     *            - The URL to visit
+     * @return whether or not the crawl was successful
      */
-    public void search(String url, String searchWord)
+    public boolean crawl(String url)
     {
-        while(this.pagesVisited.size() < MAX_PAGES_TO_SEARCH)
+        try
         {
-            String currentUrl;
-            SpiderLeg leg = new SpiderLeg();
-            if(this.pagesToVisit.isEmpty())
+            Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
+            Document htmlDocument = connection.get();
+            this.htmlDocument = htmlDocument;
+            if(connection.response().statusCode() == 200) // 200 is the HTTP OK status code
+            // indicating that everything is great.
             {
-                currentUrl = url;
-                this.pagesVisited.add(url);
+                System.out.println("\n**Visiting** Received web page at " + url);
             }
-            else
+            if(!connection.response().contentType().contains("text/html"))
             {
-                currentUrl = this.nextUrl();
+                System.out.println("**Failure** Retrieved something other than HTML");
+                return false;
             }
-            leg.crawl(currentUrl); // Lots of stuff happening here. Look at the crawl method in
-            // SpiderLeg
-            boolean success = leg.searchForWord(searchWord);
-            if(success)
+            Elements linksOnPage = htmlDocument.select("a[href]");
+            System.out.println("Found (" + linksOnPage.size() + ") links");
+            for(Element link : linksOnPage)
             {
-                System.out.println(String.format("**Success** Word %s found at %s", searchWord, currentUrl));
-                break;
+                this.links.add(link.absUrl("href"));
             }
-            this.pagesToVisit.addAll(leg.getLinks());
+            return true;
         }
-        System.out.println("\n**Done** Visited " + this.pagesVisited.size() + " web page(s)");
-    }
-
-
-    /**
-     * Returns the next URL to visit (in the order that they were found). We also do a check to make
-     * sure this method doesn't return a URL that has already been visited.
-     *
-     * @return
-     */
-    private String nextUrl()
-    {
-        String nextUrl;
-        do
+        catch(IOException ioe)
         {
-            nextUrl = this.pagesToVisit.remove(0);
-        } while(this.pagesVisited.contains(nextUrl));
-        this.pagesVisited.add(nextUrl);
-        return nextUrl;
+            // We were not successful in our HTTP request
+            return false;
+        }
     }
+
+    public List<String> getLinks()
+    {
+        return this.links;
+    }
+
 }
--- src/SpiderTest.java
+++ src/SpiderTest.java
@@ -9,6 +9,6 @@ public class SpiderTest
     public static void main(String[] args)
     {
         Spider spider = new Spider();
-        spider.search("http://arstechnica.com", "computer");
+        spider.crawl("http://arstechnica.com");
     }
 }
