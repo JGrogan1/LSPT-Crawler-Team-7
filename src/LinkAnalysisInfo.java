9a85b000eec9a4a38991e3dfe507c815a66e84ef
Joe Grogan <grogaj@rpi.edu>
2017-11-30 15:43:24 -0500

    Not done

--- src/LinkAnalysisInfo.java
+++ src/LinkAnalysisInfo.java
@@ -1,3 +1,22 @@
-public class LinkAnalysisInfo {
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkAnalysisInfo
+{
+    private String link;
+    private String dateLastUpdated;
+    private String frequency;
+    private List<String> outlinks;
+    private List<String> failedWebpages;
+
+    public LinkAnalysisInfo(String link, String dateLastUpdated, String frequency, List<String> outlinks, List<String> failedWebpages)
+    {
+        this.link = link;
+        this.dateLastUpdated = dateLastUpdated;
+        this.frequency = frequency;
+        this.outlinks = new ArrayList<String>(outlinks);
+        this.failedWebpages = new ArrayList<String>(failedWebpages);
+    }
+
 
 }
