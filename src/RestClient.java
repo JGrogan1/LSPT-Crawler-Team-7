3338ab504718cacc7139172e57cf9be6bc1c6220
John Steigerwald <johnstiggy@gmail.com>
2017-12-07 15:19:38 -0500

    Updated the Rest Client for Text Transformation

--- src/RestClient.java
+++ src/RestClient.java
@@ -19,14 +19,35 @@ public class RestClient {
         i = this;
     }
 
-    public String sendLinkAnalysisPost() {
-        JSONArray array = new JSONArray(DatabaseHandler.i.GetAll());
+    public String LinkAnalysisPost(DatabaseInfo dbi) {
+        List<DatabaseInfo> pages = new LinkedList<DatabaseInfo>();
+        pages.add(dbi);
+        JSONArray array = new JSONArray(pages);
         List<String> dead_links = new LinkedList<>();
         JSONObject json = new JSONObject();
         json.put("webpages",array);
         json.put("failedWebpages",dead_links);
         System.out.println("Sending to LinkAnalysis: " + json.toString());
-        //DatabaseHandler.i.db.drop(); //Remove all old data from the table
+        try {
+            HttpResponse<String> httpResponse = Unirest.post(URI)
+                    .header("Content-Type", "application/json")
+                    .body(json.toString()).asString();
+            return "ack";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "failure";
+        }
+    }
+
+    public String sendTextTransformationPost(DatabaseInfo dbi) {
+        JSONObject json = new JSONObject();
+        json.put("html",dbi.getHtml());
+        json.put("docs",dbi.getDocuments());
+        json.put("outlinks",dbi.getDocuments());
+        json.put("url",dbi.getDocuments());
+        //json.put("timestamp",);
+
+        System.out.println("Sending to Text Transformation: " + json.toString());
         try {
             HttpResponse<String> httpResponse = Unirest.post(URI)
                     .header("Content-Type", "application/json")
