ad18e9acbaba4888a39b35d7fc186dd8861fb94f
Logan Garber <Logangarber@hotmail.com>
2017-12-07 14:26:52 -0500

    added text document class

---
+++ src/TextDocument.java
@@ -0,0 +1,17 @@
+public class TextDocument {
+    public String getLink() {
+        return link;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    private String link;
+    private String text;
+    private String extension;
+}
